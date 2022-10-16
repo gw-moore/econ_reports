1cda1adfecbf664dcbd27b2b3f5cda728a877cea
gw-moore <gwmoore92@gmail.com>
2022-10-16 00:00:00 -0400

    Add more functionality to SeriesCollection

--- econ_reports/utils/pyfredapi.py
+++ econ_reports/utils/pyfredapi.py
@@ -1,7 +1,7 @@
 """Utility functions for pyfredapi. If I like something enough I may move it into pyfredapi.
 """
 
-from typing import List
+from typing import List, Union
 
 from pyfredapi import FredSeries
 from rich.console import Console
@@ -10,27 +10,80 @@ console = Console()
 
 
 class SeriesCollection:
-    """A collection of pyfredapi.SeriesData objects."""
+    """A collection of pyfredapi.SeriesData objects.
 
-    def __init__(self, client: FredSeries, series: List[str]):
+    Parameters
+    ----------
+    client : FredSeries
+        A FredSeries object.
+    """
+
+    def __init__(self, client: FredSeries = FredSeries()):
         self.client = client
-        self.series = series
+        self.responses = {}
+        self._source = {}
 
-        responses = {}
-        for s in series:
-            print(f"Requesting series {s}...")
-            response = client.get_series(series_id=s)
-            responses[s] = response
+    def add_series(
+        self,
+        series: Union[str, List[str]],
+        drop_realtime: bool = True,
+        set_col_name_to_series_id: bool = True,
+    ) -> None:
+        """Add series to class instance.
 
-        for series_name, series_data in responses.items():
-            setattr(self, series_name, series_data)
+        Parameters
+        ----------
+        series : Union[str, List[str]]
+            Series to add to collection.
+        drop_realtime : bool
+            Indicates if you want to drop the realtime columns.
+        set_col_name_to_series_id : bool
+            Indicates if you want to rename the value column to series id.
+        """
+        if isinstance(series, str):
+            series = [series]
 
-        self.responses = self.__dict__.copy()
-        _ = self.responses.pop("client", None)
-        _ = self.responses.pop("series", None)
+        for series_name in series:
+            if series_name in self.responses:
+                print(f"Already have {series_name}")
+            else:
+                print(f"Requesting series {series_name}...")
+                response = self.client.get_series(series_id=series_name)
+                if drop_realtime:
+                    response.data.drop(
+                        ["realtime_start", "realtime_end"], inplace=True, axis=1
+                    )
+                if set_col_name_to_series_id:
+                    response.data.rename(columns={"value": series_name}, inplace=True)
+                self.responses[series_name] = response
+                setattr(self, series_name, response)
+
+    def drop_series(self, series: Union[str, List[str]]) -> None:
+        """Drop series from collection.
+
+        Parameters
+        ----------
+        series : Union[str, List[str]]
+            Series to remove from collection.
+        """
+        if isinstance(series, str):
+            series = [series]
+
+        for series_name in series:
+            try:
+                del self.responses[series_name]
+            except KeyError:
+                print(f"No series {series_name} in collection")
+            delattr(self, series_name)
+            print(f"Removed series {series_name}")
+
+    def list_series(self):
+        """List the series in the collection."""
+        for series_data in list(self.responses.values()):
+            console.print(f"{series_data.info.id}: {series_data.info.title}")
 
     def show_seasonality(self) -> None:
-        """"""
+        """Show seasonality of the series."""
         seasonal_adjustments = [
             series_data.info.seasonal_adjustment
             for series_data in list(self.responses.values())
@@ -48,7 +101,7 @@ class SeriesCollection:
                     console.print(f"{series_data.info.id}: {series_data.info.title}")
 
     def show_frequency(self) -> None:
-        """"""
+        """Show frequency of the series."""
         frequencies = [
             series_data.info.frequency for series_data in list(self.responses.values())
         ]
@@ -65,7 +118,7 @@ class SeriesCollection:
                     console.print(f"{series_data.info.id}: {series_data.info.title}")
 
     def show_observation_end(self) -> None:
-        """"""
+        """Show the latest date of the series."""
         end_dates = [
             series_data.info.observation_end
             for series_data in list(self.responses.values())
@@ -85,15 +138,3 @@ class SeriesCollection:
     def extract_series(self):
         """Return a new instance of SeriesCollection, with a subset of data from this instance."""
         raise NotImplementedError
-
-
-def get_series(client: FredSeries, series: List[str]) -> SeriesCollection:
-    """Take a list of FRED series and returns a SeriesCollection."""
-
-    responses = {}
-    for s in series:
-        print(f"Requesting series {s}...")
-        response = client.get_series(series_id=s)
-        responses[s] = response
-
-    return SeriesCollection(**responses)
